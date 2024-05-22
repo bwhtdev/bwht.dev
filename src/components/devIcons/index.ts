0c125b302295f2e6aa7d1828d22256d8f8e5cccc
Brad White <bradscottwhite@gmail.com>
2024-05-22 11:13:35 -0400

    added projects

---
+++ src/components/devIcons/index.ts
@@ -0,0 +1,19 @@
+import TypeScriptIcon from './TypeScriptIcon.ts';
+import AstroIcon from './AstroIcon.ts';
+import ReactIcon from './ReactIcon.ts';
+import TailwindIcon from './TailwindIcon.ts';
+import GoIcon from './GoIcon.ts';
+import PostgresIcon from './PostgresIcon.ts';
+import AngularIcon from './AngularIcon.ts';
+import JavaIcon from './JavaIcon.ts';
+
+export {
+  TypeScriptIcon,
+  AstroIcon,
+  ReactIcon,
+  TailwindIcon,
+  GoIcon,
+  PostgresIcon,
+  AngularIcon,
+  JavaIcon
+};
